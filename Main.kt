f585b41f062fbb37ce6b09a271ba0d329497cca5
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-02-24 12:08:31 -0300

    okok

--- Main.kt
+++ Main.kt
@@ -1,8 +1,15 @@
 
+val prop_modificavel : String = "modificavel"
+
+fun interpolacao_string_com_variavel(texto: String): String {
+  return "ola $texto"
+}
+
 fun hello(): String {
   return "Hello Word"
 }
 
 fun main() {
-  println(hello())
+  // println(hello())
+  println(interpolacao_string_com_variavel(prop_modificavel))
 }
---
+++ foo/1/1.kt
@@ -0,0 +1,8 @@
+
+fun foo1(): String {
+  return "======= Foo 1 ta distante ------- "
+}
+
+fun main() {
+  println(foo1())
+}
